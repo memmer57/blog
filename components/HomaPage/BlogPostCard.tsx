f7b2efe837c808598899fc4e8156d7c53d894f0b
memmer57 <matej.memmer@gmail.com>
2023-11-26 19:30:55 +0100

    tags

--- components/HomaPage/BlogPostCard.tsx
+++ components/HomaPage/BlogPostCard.tsx
@@ -10,6 +10,13 @@ export default function BlogPostCard({ post }: { post: IPost }) {
   return (
     <Link href={`/posts/${post.attributes.slug}`} className="blog-post-card">
       <h2>{post.attributes.title}</h2>
+      <div className="tag-container">
+        {post.attributes.tags.data.map((tag) => (
+          <span key={tag.id} className="tag">
+            {tag.attributes.name}
+          </span>
+        ))}
+      </div>
       <div className="post-info-container">
         <p>{formattedDate}</p>
         <p>
