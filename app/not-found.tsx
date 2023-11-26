01042d261f2737cd340c09dc06620507b3de83de
memmer57 <matej.memmer@gmail.com>
2023-11-26 19:19:17 +0100

    styling tweaks

---
+++ app/not-found.tsx
@@ -0,0 +1,5 @@
+import FourOnFour from "@/components/404/404"
+
+export default function NotFoundPage() {
+  return <FourOnFour />
+}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,14 +4,18 @@ import Link from "next/link"
 const Navbar = () => {
   return (
     <nav>
-      <Link href="/" className="logo">
-        Memmer's blog
-      </Link>
+      <div className="nav-content">
+        <Link href="/" className="logo">
+          Memmerovo
+        </Link>
+      </div>
+      {/*
       <div className="nav-links">
         <div className="nav-link">
           <Link href="/">Home</Link>
         </div>
       </div>
+      */}
     </nav>
   )
 }
