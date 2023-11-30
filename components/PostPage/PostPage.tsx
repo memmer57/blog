124b0f0e4e1a5b27d9c2bfda232398236e48913d
HangerThem <f.borisjuk@gmail.com>
2023-11-30 21:37:37 +0100

    Reskin

--- components/PostPage/PostPage.tsx
+++ components/PostPage/PostPage.tsx
@@ -1,38 +1,61 @@
+"use client"
+
 import { requestAllPublishedPosts } from "@/services/api-services/blogService"
 import "./PostPage.scss"
 import { remark } from "remark"
 import html from "remark-html"
-import { notFound } from "next/navigation"
+import { useEffect, useState } from "react"
+import { useTheme } from "@/context/themeContext"
 
-export default async function PostPage({
-  params,
-}: {
-  params: { post: string }
-}) {
-  try {
-    const postsResponse = await requestAllPublishedPosts()
+export default function PostPage({ params }: { params: { post: string } }) {
+  const [post, setPost] = useState<any>(null)
+  const [postContentHtml, setPostContentHtml] = useState<string>("")
 
-    const posts = postsResponse.posts
-    const post = posts.find((post) => post.attributes.slug === params.post)
+  const { theme } = useTheme()
 
-    if (!post) {
-      return notFound()
-    }
+  useEffect(() => {
+    requestAllPublishedPosts().then((res) => {
+      const post = res.posts.find(
+        (post: any) => post.attributes.slug === params.post
+      )
+      setPost(post)
+    })
+  }, [])
 
-    const processedContent = await remark()
+  useEffect(() => {
+    if (!post) return
+    remark()
       .use(html)
-      .process(post.attributes.content)
-    const postContentHtml = processedContent.toString()
+      .process(post.attributes.content, function (err, file) {
+        if (err) throw err
+        setPostContentHtml(String(file))
+      })
+  }, [post])
 
-    return (
-      <div className="postpage">
-        <div className="post-wrapper">
-          <h1>{post.attributes.title}</h1>
-          <div dangerouslySetInnerHTML={{ __html: postContentHtml }} />
+  if (!post) return null
+
+  return (
+    <div className="postpage">
+      <div className={`post-wrapper ${theme}`}>
+        <h1>{post.attributes.title}</h1>
+        <div className="post-info-container">
+          <div className="tag-container">
+            {post.attributes.tags.data.map((tag: any) => (
+              <span
+                key={tag.id}
+                className="tag"
+                style={{ backgroundColor: tag.attributes.color }}
+              >
+                {tag.attributes.name}
+              </span>
+            ))}
+          </div>
+          <div className="post-date">
+            {new Date(post.attributes.publishedAt).toLocaleDateString()}
+          </div>
         </div>
+        <div dangerouslySetInnerHTML={{ __html: postContentHtml }} />
       </div>
-    )
-  } catch (error) {
-    return notFound()
-  }
+    </div>
+  )
 }
