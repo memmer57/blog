e8a65414907cbd0b2331659fb557ec43c7cd9219
memmer57 <matej.memmer@gmail.com>
2023-12-02 00:53:29 +0100

    fix typoo

--- services/requestHelpers.ts
+++ services/requestHelpers.ts
@@ -2,7 +2,7 @@ import { API_URL } from "./servicesConfig"
 
 export async function get<Res>(url: string): Promise<Res> {
   return (
-    await fetch(API_URL + url, {
+    await fetch("https://blog-git-mrttnx-memmer57.vercel.app/api" + url, {
       method: "GET",
       cache: "no-store",
       headers: {
