3dc6eb708c7afb018ee9870e20e98a954dbdd92a
memmer57 <matej.memmer@gmail.com>
2023-12-02 00:46:22 +0100

    delete unused fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.scss"
 import type { Metadata } from "next"
-import { Inter, Lato, Montserrat, Nunito, Poppins } from "next/font/google"
+import { Poppins } from "next/font/google"
 import { Analytics } from "@vercel/analytics/react"
 import Navbar from "@/components/Navbar"
 import { ThemeProvider } from "@/context/themeContext"
